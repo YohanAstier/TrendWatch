233d7ff47d9ebadc1922513faf5b9608f090babf
Ayoub Alloune <ayouballoune@hotmail.com>
2023-01-07 16:54:49 +0100

    cleaned .env variables loading

--- makefile
+++ makefile
@@ -1,5 +1,8 @@
-#INSTALL REQUIRED PYTHON MODULES
-pip install -r requirements.txt
+SHELL := /bin/bash
 
-#LOAD ENV VARIABLES
-source .env
+all:
+
+	#INSTALL REQUIRED PYTHON MODULES
+	pip install -r requirements.txt
+	#LOAD ENV VARIABLES
+	source .env
---
+++ scrap.py
@@ -0,0 +1,26 @@
+# For sending GET requests from the API
+import requests
+# For saving access tokens and for file management when creating and adding to the dataset
+import os
+# For dealing with json responses we receive from the API
+import json
+# For displaying the data after
+import pandas as pd
+# For saving the response data in CSV format
+import csv
+# For parsing the dates received from twitter in readable formats
+import datetime
+import dateutil.parser
+import unicodedata
+#To add wait time between requests
+import time
+#For loading .env
+from dotenv import load_dotenv
+
+#load sourced environment variables
+load_dotenv()
+
+#read environment variable
+TWITTER_KEY = os.getenv('TWITTER_KEY')
+
+print(TWITTER_KEY)
