5bf87225982e7265d75cde3726862033e7ee0a04
YohanAstier <yohan.astier@edu.esiee.fr>
2023-01-07 16:22:02 +0100

    first connection to Twitter API

---
+++ TweetScraping.py
@@ -0,0 +1,49 @@
+import json
+import os
+
+import tweepy
+import requests
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAPxNlAEAAAAA0RitAk2TVLO38sj%2BDG%2FCeU1np5g%3DGNCaXaN5Fi2nNzkTnRu0gJI99yhkdghxjWFmqqo0E2GgUanfKs"
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
+def create_url(keyword, start_date, end_date, max_results=10):
+    search_url = "https://api.twitter.com/2/tweets/search/all"
+
+    # change params based on the endpoint you are using
+    query_params = {'query': keyword,
+                    'start_time': start_date,
+                    'end_time': end_date,
+                    'max_results': max_results,
+                    'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
+                    'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+                    'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
+                    'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
+                    'next_token': {}}
+    return (search_url, query_params)
+
+
+def connect_to_endpoint(url, headers, params, next_token = None):
+    params['next_token'] = next_token   #params object received from create_url function
+    response = requests.request("GET", url, headers = headers, params = params)
+    print("Endpoint Response Code: " + str(response.status_code))
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+if __name__ == "__main__":
+    headers = create_headers(bearer_token)
+    keyword = "xbox lang:en"
+    start_time = "2021-03-01T00:00:00.000Z"
+    end_time = "2021-03-31T00:00:00.000Z"
+    max_results = 15
+
+    url = create_url(keyword, start_time, end_time, max_results)
+    json_response = connect_to_endpoint(url[0], headers, url[1])
+
+    print(json.dumps(json_response, indent=4, sort_keys=True))
