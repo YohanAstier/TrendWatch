1c9d84ba7b0f0b2d28d9ebbbbd3d3fc8dbc473be
YohanAstier <yohan.astier@edu.esiee.fr>
2023-03-16 17:06:26 +0100

    prettier AI Class

--- AI/AI.py
+++ AI/AI.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import pickle
-import gzip
 from enum import Enum
 
-
-# Load Data Viz Pkgs
-import seaborn as sns
 
 # Load Text Cleaning Pkgs
 import neattext.functions as nfx
@@ -26,65 +22,73 @@ class Mode(Enum):
     NONE = 0
     POSORNEG = 1
     EMOTIONCLASS = 2
+
+class AI():
+    def __init__(self, model_path = None, mode = Mode.NONE):
+        self.load_model(model_path, mode)
+        
+
+    def train_AI():
+        df = pd.read_csv("datasets/dataset.csv", delimiter=';')
+        dir(nfx)
+        print(df.columns)
+        df['text'] = df['text'].fillna("").astype('string')
+        df['Clean_Text'] = df['text'].apply(nfx.remove_userhandles)
+        df['Clean_Text'] = df['Clean_Text'].apply(nfx.remove_stopwords)
+
+        vectorizer = TfidfVectorizer()
+        Xfeatures = vectorizer.fit_transform(df['Clean_Text'].values.astype('str'))
+        ylabels = df['emotion']
+
+        x_train, x_test, y_train, y_test = train_test_split(Xfeatures, ylabels, test_size=0.1, random_state=42)
+
+        model = LogisticRegression(max_iter=1_000_000)
+        model = model.fit(x_train, y_train)
+
+        accuracy = model.score(x_test, y_test)
+        print(f"Précision du modèle: {str('%.2f' % (accuracy * 100))}%")
+
+        pipeline_file = open("/models/bad_or_good_ai.pkl", "wb")
+        pickle.dump(model, pipeline_file)
+        pipeline_file.close()
+
+        return model
+
+    def train_Emotion_AI():
+        df = pd.read_csv("datasets/emotion_dataset_raw.csv")
+        dir(nfx)
+        df['Clean_Text'] = df['Text'].apply(nfx.remove_userhandles)
+        df['Clean_Text'] = df['Clean_Text'].apply(nfx.remove_stopwords)
+        Xfeatures = df['Clean_Text']
+        ylabels = df['Emotion']
+        x_train, x_test, y_train, y_test = train_test_split(Xfeatures, ylabels, test_size=0.3, random_state=42)
+        
+        pipe_lr = Pipeline(steps=[('cv', CountVectorizer()), ('lr', LogisticRegression(max_iter=1_000_000))])
+        pipe_lr.fit(x_train, y_train)
+        accuracy = pipe_lr.score(x_test, y_test)
+        print(f"Précision du modèle: {str('%.2f' % (accuracy * 100))}%")
+
+
+        pipeline_file = open("models/class_emotion_ai.pkl", "wb")
+        pickle.dump(pipe_lr, pipeline_file)
+        pipeline_file.close()
+
+    def load_model(self, model_path, target_mode):
+        self.mode = target_mode            
+        if target_mode == Mode.NONE :
+            self.model = None
+
+        else :
+            self.model = pickle.load(open(model_path, 'rb'))
+
+    def predict_message(self, message, model_path, target_mode):
+        if self.mode != Mode.NONE: 
+            return self.model.predict_proba([message])
+        return []
     
-model = None
-mode = Mode.NONE
+    def emotions_label(self):
+        if self.mode != Mode.NONE:
+            return list(self.model.classes_)
+        return []
 
-
-def Train_AI():
-    df = pd.read_csv("datasets/dataset.csv", delimiter=';')
-    dir(nfx)
-    print(df.columns)
-    df['text'] = df['text'].fillna("").astype('string')
-    df['Clean_Text'] = df['text'].apply(nfx.remove_userhandles)
-    df['Clean_Text'] = df['Clean_Text'].apply(nfx.remove_stopwords)
-
-    vectorizer = TfidfVectorizer()
-    Xfeatures = vectorizer.fit_transform(df['Clean_Text'].values.astype('str'))
-    ylabels = df['emotion']
-
-    x_train, x_test, y_train, y_test = train_test_split(Xfeatures, ylabels, test_size=0.1, random_state=42)
-
-    model = LogisticRegression(max_iter=1_000_000)
-    model = model.fit(x_train, y_train)
-
-    predictions = model.predict(x_test)
-    accuracy = model.score(x_test, y_test)
-    print(f"Précision du modèle: {str('%.2f' % (accuracy * 100))}%")
-
-    pipeline_file = open("/models/bad_or_good_ai.pkl", "wb")
-    pickle.dump(model, pipeline_file)
-    pipeline_file.close()
-
-    return model
-
-
-def Train_Emotion_AI():
-    df = pd.read_csv("datasets/emotion_dataset_raw.csv")
-    dir(nfx)
-    df['Clean_Text'] = df['Text'].apply(nfx.remove_userhandles)
-    df['Clean_Text'] = df['Clean_Text'].apply(nfx.remove_stopwords)
-    Xfeatures = df['Clean_Text']
-    ylabels = df['Emotion']
-    x_train, x_test, y_train, y_test = train_test_split(Xfeatures, ylabels, test_size=0.3, random_state=42)
-    
-    pipe_lr = Pipeline(steps=[('cv', CountVectorizer()), ('lr', LogisticRegression(max_iter=1_000_000))])
-    pipe_lr.fit(x_train, y_train)
-    accuracy = pipe_lr.score(x_test, y_test)
-    print(f"Précision du modèle: {str('%.2f' % (accuracy * 100))}%")
-
-
-    pipeline_file = open("models/class_emotion_ai.pkl", "wb")
-    pickle.dump(pipe_lr, pipeline_file)
-    pipeline_file.close()
-
-def load_and_test(message, model_path, target_mode):
-    global model
-    global mode
-
-    if mode != target_mode :
-        model = pickle.load(open(model_path, 'rb'))
-        mode = target_mode
-
-    return model.predict_proba([message])
 
--- TestAI.py
+++ TestAI.py
@@ -12,7 +12,11 @@ if __name__ == "__main__":
     tweetsText = [message['text'] for message in data['data'] ]
     # Iterating through the json
 
+    ai = AIFunc.AI(mode=AIFunc.Mode.EMOTIONCLASS , model_path="./models/class_emotion_ai.pkl")
+    labels = ai.emotions_label()
     # AIFunc.Train_Emotion_AI()
-    print([AIFunc.load_and_test(tweet, "./models/class_emotion_ai.pkl", AIFunc.Mode.EMOTIONCLASS) for tweet in tweetsText])
+    print([ai.predict_message(tweet) for tweet in tweetsText])
+    
 
     print("end")
+
